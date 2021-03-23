ea853d1a9161c62bfc7d11c327ba9f9fc5f17ba6
Alex Rybachev <rybachev.as@gmail.com>
2021-03-24 00:14:07 +0400

    Initial Commit

---
+++ ManualTrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualTrafficLights
+//
+//  Created by Aleksandr Rybachev on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
